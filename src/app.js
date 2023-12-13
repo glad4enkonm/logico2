871d6242c88c418df0e8bb2d6bdeadb125a480de
glad4enkonm <glad4enkonm@gmail.com>
2023-12-13 23:57:37 +0300

    on click +, dark theme +

--- src/app.js
+++ src/app.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
-const data = {
+const data = { nodes: [...Array(30)].map((_, i) => ({ id: `node${i + 1}`, label: `node${i + 1}` })), edges: [...Array(60)].map(() => ({ source: `node${Math.floor(Math.random() * 30) + 1}`, target: `node${Math.floor(Math.random() * 30) + 1}`, label: `edge${Math.floor(Math.random() * 60) + 1}` })) }
+/*{
     // The array of nodes
     nodes: [
       {
-        id: 'node1', // String, unique and required
-        x: 100, // Number, the x coordinate
-        y: 200, // Number, the y coordinate
+        id: 'node1',
+        label: 'node1',
       },
       {
-        id: 'node2', // String, unique and required
-        x: 300, // Number, the x coordinate
-        y: 200, // Number, the y coordinate
+        id: 'node2',
+        label: 'node2',
       },
     ],
     // The array of edges
     edges: [
       {
-        source: 'node1', // String, required, the id of the source node
-        target: 'node2', // String, required, the id of the target node
+        source: 'node1',
+        target: 'node2',
+        label: 'node1toNode2',
       },
     ],
-  };
+  }; */
 import G6 from '@antv/g6';
+
+// Define the highlight styles for the edge and nodes
+const highlightStyle = {
+  stroke: '#03DAC6', // Highlight color for the edge        
+  // Additional styles if needed
+}
+
+const defaultStyle = {
+  stroke: "#343434", // Borders and dividers color for edges
+}, defaultNodeStyle = {
+  stroke: "#BB86FC"
+}
+
+
+const highlited = {nodes:[], edges:[]}
 
 export function App() {
   const ref = React.useRef(null)
@@ -36,7 +51,7 @@ export function App() {
     || document.body.clientHeight;
 
   useEffect(() => {
-    if(!graph) {      
+    if (!graph) {
       graph = new G6.Graph({
         container: ref.current,
         width: width * 0.8,
@@ -45,37 +60,104 @@ export function App() {
           default: ['drag-canvas', 'drag-node', 'zoom-canvas']
         },
         defaultNode: {
-            type: "circle",
-            size: [100],
-            color: "#5B8FF9",
-            style: {
-              fill: "#9EC9FF",
-              lineWidth: 3
-            },
-            labelCfg: {
-              style: {
-                fill: "#fff",
-                fontSize: 20
-              }
-            }
+          type: "circle",
+          size: [100],
+          color: "#BB86FC", // Accent Color for the border of the nodes
+          style: {
+            fill: "#121212", // Dark background color for nodes
+            lineWidth: 3,
+            stroke: "#BB86FC", // Optional: if you want the node border to have the accent color
           },
-          defaultEdge: {
+          labelCfg: {
             style: {
-              stroke: "#e2e2e2"
+              fill: "#E0E0E0", // Primary text color for labels
+              fontSize: 20
             }
           }
-      })
+        },
+        defaultEdge: {
+          style: {
+            stroke: "#343434", // Borders and dividers color for edges
+          },
+          labelCfg: {
+            style: {
+              fill: "#E0E0E0", // Primary text color for labels
+              fontSize: 20
+            }
+          }
+        },
+      });
     }
-    
-    graph.data(data)
+
+    graph.on('edge:click', (e) => {
+      highlited.edges.push(e.item)      
+      highlited.nodes.push(e.item.getSource(), e.item.getTarget())      
+
+      const elements = highlited.edges.concat(highlited.nodes)
+      elements.forEach(el => graph.updateItem(el, {
+        style: highlightStyle,
+      }))
+
+      graph.paint();
+    });
+
+    graph.on('node:click', (e) => {
+      const node = e.item; // The clicked node
+      highlited.nodes.push(node);
+      
+      // Get connected edges for the clicked node
+      const edges = graph.getEdges().filter(edge => {
+          return edge.getSource() === node || edge.getTarget() === node;
+      });
   
-    graph.render()    
+      // Add the connected edges to the highlighted list
+      edges.forEach(edge => {
+          highlited.edges.push(edge);
+          
+          // Additionally, highlight the opposite node of each edge
+          const source = edge.getSource();
+          const target = edge.getTarget();
+          if (source !== node && highlited.nodes.indexOf(source) === -1) {
+              highlited.nodes.push(source);
+          }
+          if (target !== node && highlited.nodes.indexOf(target) === -1) {
+              highlited.nodes.push(target);
+          }
+      });
+  
+      // Apply the highlight style to all highlighted elements
+      const elements = highlited.edges.concat(highlited.nodes);
+      elements.forEach(el => graph.updateItem(el, {
+          style: highlightStyle,
+      }));
+  
+      graph.paint();
+  });
+
+    // Optional: Reset styles when clicking elsewhere on the canvas
+    graph.on('canvas:click', () => {
+      highlited.edges.forEach(el => graph.updateItem(el, {
+        style: defaultStyle,
+      }))
+
+      highlited.nodes.forEach(el => graph.updateItem(el, {
+        style: defaultNodeStyle,
+      }))
+
+      highlited.edges = highlited.nodes = []
+
+      graph.paint();
+    });
+
+    graph.data(data)
+
+    graph.render()
     graph.paint()
-    
+
   }, [])
 
   return (
-    <div ref={ref}>      
+    <div ref={ref}>
     </div>
   );
 }
